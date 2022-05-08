b00024aa4dcb825a1e3b34c20dde89024b8ac43a
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-05-08 19:02:05 +0100

    rename searchResult to MovieList and add dummy ListItems

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function MovieList() {
+  return (
+    <div className="movie__container">
+      <p className="movieList__title">Results from searchResult</p>
+      <ul className="movieList">
+        <li>
+          search result <button>nominate</button>
+        </li>
+        <li>
+          search result <button>nominate</button>
+        </li>
+        <li>
+          search result <button>nominate</button>
+        </li>
+      </ul>
+    </div>
+  )
+}
