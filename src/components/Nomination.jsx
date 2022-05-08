e535ce7055387e8e2d606aaf7ba1ad8606019f65
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-05-08 19:34:26 +0100

    add return statement and items

--- src/components/Nomination.jsx
+++ src/components/Nomination.jsx
@@ -1,5 +1,14 @@
 import React from "react"
 
 export default function Nomination() {
-  return <div>Nomination</div>
+  return (
+    <div className="nomination">
+      <p className="nomination__title">Nomination</p>
+      <ul className="nominations">
+        <li>
+          nominee <button>remove</button>
+        </li>
+      </ul>
+    </div>
+  )
 }
