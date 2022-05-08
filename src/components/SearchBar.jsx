ed4265cb624c8e05e041e4873557e637bc376cf2
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-05-08 18:53:51 +0100

    add search bar icon and update SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,14 +4,29 @@ export default function SearchBar() {
   return (
     <div className="SearchBar">
       <label htmlFor="search">
-        Movie title
+        <p className="search__title">Movie Title</p>
         <span className="search__input--iconed">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            class="feather feather-search search__input--icon"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+          </svg>
           <input
             type="search"
             name="search"
             id="search"
-            value="ram"
             className="search__input"
+            results="2"
           />
         </span>
       </label>
