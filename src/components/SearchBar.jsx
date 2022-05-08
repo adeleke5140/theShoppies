4dbf4ca46fd664b9c0615b1d2cf13fcc2a3c89a9
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-05-08 19:41:16 +0100

    change class to className

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -16,7 +16,7 @@ export default function SearchBar() {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            class="feather feather-search search__input--icon"
+            className="feather feather-search search__input--icon"
           >
             <circle cx="11" cy="11" r="8"></circle>
             <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
