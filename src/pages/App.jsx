e0078e15d83db0b3b0afe11ef04846543ecf3243
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-05-09 02:38:34 +0100

    remove all reactQuery imports

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,20 +1,17 @@
-import { QueryClient, QueryClientProvider } from "react-query"
+// import { QueryClient, QueryClientProvider } from "react-query"
 
-import { ReactQueryDevtools } from "react-query/devtools"
+// import { ReactQueryDevtools } from "react-query/devtools"
 
-const queryClient = new QueryClient()
+// const queryClient = new QueryClient()
 import Header from "../components/Header.jsx"
 import Main from "../components/Main.jsx"
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <Header title="the shoppies" />
-        <Main />
-      </div>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <div className="App">
+      <Header title="the shoppies" />
+      <Main />
+    </div>
   )
 }
 
