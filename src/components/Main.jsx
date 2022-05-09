fe36422fcca358fc1d0116748d33cd3dae192c29
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-05-09 02:39:26 +0100

    fetch data with axios, and add related functions

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,45 +2,55 @@ import SearchBar from "./SearchBar"
 import MovieList from "./MovieList"
 import Nomination from "./Nomination"
 
-import { useQuery } from "react-query"
+import axios from "axios"
 import { useEffect, useState } from "react"
 
 const api_key = import.meta.env.VITE_API_KEY
-// async function fetchMovie() {
-//   const res = await fetch()
-//   return res.json()
-// }
 
 const Main = () => {
+  const [movies, setMovies] = useState([])
   const [input, setInput] = useState("")
-  const { data, status } = useQuery(
-    "movies",
-    (fetchMovie = (input) =>
-      fetch(
-        `http://www.omdbapi.com/?apikey=${api_key}&s=${input}&type=movie`
-      ).then((res) => res.json()))
-  )
+  const [nominatedMovies, setnominatedMovies] = useState([])
 
+  //fetch movies based on the search Input
+  const getMovies = async (input) => {
+    const searchText = input.toLowerCase()
+    const url = `http://www.omdbapi.com/?apikey=${api_key}&s=${searchText}&type=movie`
+    axios.get(url).then((res) => {
+      if (res.data.Search) {
+        setMovies(res.data.Search)
+      }
+    })
+  }
+
+  //side effect to call the api on first render,
+  //and a depenedency that refetches the data when the input changes
   useEffect(() => {
-    fetchMovie(input)
+    getMovies(input)
+    console.log(movies)
   }, [input])
 
-  if (status === "loading") {
-    console.log("loading")
+  const updateInput = (event) => setInput(event.target.value)
+
+  const addNomination = (movie) => {
+    //get the latest added movie and include it to the added movie array with other nominated items
+    const newNomination = [...nominatedMovies, movie]
+    setnominatedMovies(newNomination)
   }
 
-  if (status === "error") {
-    console.error(error, "there was an error")
+  const removeNomination = (id) => {
+    const newNomination = nominatedMovies.filter((_, index) => index !== id)
+    setnominatedMovies(newNomination)
   }
-
-  console.log(data)
-  const updateInput = (e) => setInput(e.target.value)
 
   return (
     <div className="main">
       <SearchBar input={input} updateInput={updateInput} />
-      <MovieList input={input} />
-      <Nomination />
+      <MovieList input={input} movies={movies} nominate={addNomination} />
+      <Nomination
+        nominations={nominatedMovies}
+        removeNomination={removeNomination}
+      />
     </div>
   )
 }
