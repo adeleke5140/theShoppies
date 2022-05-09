a4662f55cb07fbecd93c76a2670e2f3ad825a10c
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-05-09 15:24:27 +0100

    remove toast notification for nomination

--- src/components/Nomination.jsx
+++ src/components/Nomination.jsx
@@ -1,13 +1,54 @@
-import React from "react"
+import { useEffect } from "react"
+import toast from "react-hot-toast"
 
-export default function Nomination() {
+export default function Nomination({ nominations, removeNomination }) {
+  const notify = () =>
+    toast.success("5 nominations added", {
+      duration: 4000,
+
+      style: {
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontSize: "10px"
+      }
+    })
+
+  const notifyRemoval = () => {
+    toast.error("Nomination removed", {
+      duration: 2000,
+      style: {
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontSize: "10px"
+      }
+    })
+  }
+
+  useEffect(() => {
+    const sendNotification = () => notify()
+
+    nominations.length === 5
+      ? sendNotification()
+      : `it is just ${nominations.length} nominations right now`
+  }, [nominations.length])
+
   return (
     <div className="nomination">
       <p className="nomination__title">Nomination</p>
       <ul className="nominations">
-        <li>
-          nominee <button>remove</button>
-        </li>
+        {nominations.map((nominee, id) => (
+          <li className="nominations__item" key={nominee.imdbID}>
+            {nominee.Title} ({nominee.Year}) {id}
+            <button
+              onClick={() => {
+                removeNomination(id)
+                notifyRemoval()
+              }}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   )
